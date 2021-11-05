2ec12a2eed127f92060932c4290bdebe5ce52b9a
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-05 16:10:58 +0100

    auth0

--- ios/PROJETRN/AppDelegate.h
+++ ios/PROJETRN/AppDelegate.h
@@ -4,5 +4,10 @@
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options
+{
+  return [RCTLinkingManager application:app openURL:url options:options];
+}
 
 @end
